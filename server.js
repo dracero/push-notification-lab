054cc1dbadad7760c15dc13d5cdf4e6a6b9b644c
root <diego.racero@gmail.com>
2019-04-19 21:04:04 -0300

    lister port arreglado

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 // This serves static files from the specified directory
 app.use(express.static(__dirname));
 
-const server = app.listen(8080, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
 
   const host = server.address().address;
   const port = server.address().port;
